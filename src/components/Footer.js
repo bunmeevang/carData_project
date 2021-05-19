117786de3a69d4f85cede74f3aa22923f35838b4
bunmeev <bunmee.vang@gmail.com>
2021-05-19 04:32:15 -0400

    got rid of some footer stuff

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -45,13 +45,6 @@ function Footer() {
         </div>
         <div className='footer-link-wrapper'>
           <div class='footer-link-items'>
-            <h2>Videos</h2>
-            <Link to='/'>Submit Video</Link>
-            <Link to='/'>Ambassadors</Link>
-            <Link to='/'>Agency</Link>
-            <Link to='/'>Influencer</Link>
-          </div>
-          <div class='footer-link-items'>
             <h2>Social Media</h2>
             <Link to='/'>Instagram</Link>
             <Link to='/'>Facebook</Link>
---
+++ src/components/pages/VinDecode.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function VinDecode() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default VinDecode
